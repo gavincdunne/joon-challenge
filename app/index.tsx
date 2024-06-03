a1c2d5700abdc8a5ff29f52efe42b2fc8c0c8b6d
gavincdunne <gavincdunne@gmail.com>
2024-06-03 12:54:19 -0700

    adding NativeWind to get quick styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
 import { Link } from 'expo-router';
-import { Text, View, Button} from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+
+// Needed for NativeWind styles to take effect on web app.
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 const AddFirstName = () => {
   return (
-    <View>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>What is your name?</Text>
+      <TextInput
+        placeholder="type name here..."
+        clearButtonMode='while-editing'
+        enterKeyHint='next'
+      />
       <Link href={'/set_gender'} asChild>
         <Button title='Next' />
       </Link>
-    
     </View>
   )
 }
